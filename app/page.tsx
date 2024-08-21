7621074a97b5a471d660a1300c606b6fac61c00c
Unsmoke <unsmoke.app@gmail.com>
2024-08-21 12:44:24 +0300

    [fix] requests page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className={"main__wrapper"}>
-      <Link href={"/requests"} className={"link"}>
+      <Link href={"/requests/all"} className={"link"}>
         La cereri
       </Link>
     </main>
